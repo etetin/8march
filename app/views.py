71b976a4528f94ae11a47c04c90e63e21ce0b0e7
Eugene <e.tetin.another@gmail.com>
2018-02-28 18:51:50 +0300

    cleaned the markup, minor fixes

--- app/views.py
+++ app/views.py
@@ -3,12 +3,16 @@ from app import settings
 from app.lib import imageHandler
 
 def index(request):
-    args = {}
+    args = {
+        'host': settings.HOST_ADRESS
+    }
+    '''
     id = request.GET.get('id')
     if id != None:
         image_handler = imageHandler.ImageHandler()
         if image_handler.alreadyExist(id):
             # TODO need to check file existing, now id - its filename
             args['image_url'] = settings.HOST_ADRESS + '/media/' + id
+    '''
 
     return render(request, 'index.html', args)
