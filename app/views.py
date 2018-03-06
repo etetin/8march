e68bd64e1c41d97102437662dd421882cdf0738f
Eugene <e.tetin.another@gmail.com>
2018-03-06 21:24:49 +0300

    minor fixes

--- app/views.py
+++ app/views.py
@@ -5,11 +5,14 @@ from app.lib.models import FacebookImage
 def index(request):
     args = {}
     id = request.GET.get('id')
-    filename = 'fb_share_static.jpg'
     if id != None and id.isnumeric() and FacebookImage.objects.filter(id=id).exists():
         filename = str(id) + '.jpg'
+        args['image_url'] = settings.MEDIA_URL + filename
+    else:
 
-    args['image_url'] = settings.MEDIA_URL + filename
+        filename = 'fb_share_static.jpg'
+        args['image_url'] = settings.STATIC_URL + 'img/' + filename
+
     return render(request, 'index.html', args)
 
 def privacy(request):
